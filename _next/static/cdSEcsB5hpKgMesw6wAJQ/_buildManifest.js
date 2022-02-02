444258d916c0c63670613f8ebd1ad512eaeaace0
Boontawat Kumpiroj <boontawatk@Boontawats-MacBook-Air.local>
2022-02-02 15:20:05 +0700

    🚀 Deploy [02/02/22]

---
+++ _next/static/cdSEcsB5hpKgMesw6wAJQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-148125897af243f8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/test":["static/chunks/pages/test-11eb066ba43adbe2.js"],sortedPages:["/","/_app","/_error","/test"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
